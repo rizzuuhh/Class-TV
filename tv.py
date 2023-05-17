74762d487eda3501660b15f4589ee5db423b5790
rizza <denostarizza43@gmail.com>
2023-05-18 00:14:57 +1200

    Pseudocode

---
+++ tv.py
@@ -0,0 +1,32 @@
+Class TV:
+    Properties:
+        channel: int
+        volumeLevel: int
+        on: bool
+
+    Methods:
+        turnOn(): None
+        turnOff(): None
+        getChannel(): int
+        setChannel(channel: int): None
+        getVolume(): int
+        setVolume(volumeLevel: int): None
+        channelUp(): None
+        channelDown(): None
+        volumeUp(): None
+        volumeDown(): None
+
+
+Create an instance of TV called tv1
+Create an instance of TV called tv2
+
+Turn on tv1
+Set tv1's channel to 30
+Set tv1's volume level to 3
+
+Turn on tv2
+Set tv2's channel to 3
+Set tv2's volume level to 2
+
+Print tv1's information
+Print tv2's information

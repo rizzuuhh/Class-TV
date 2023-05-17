aaff3ea6c51b37c86d0f49fed420b6d3bb6c1827
rizza <denostarizza43@gmail.com>
2023-05-18 00:53:31 +1200

    started with channel and volume's int

--- tv.py
+++ tv.py
@@ -1,32 +1,36 @@
-Class TV:
-    Properties:
-        channel: int
-        volumeLevel: int
-        on: bool
+#Class TV:
+    #Properties:
+        #channel: int
+        #volumeLevel: int
+        #on: bool
+def __init__(self):
+        self.channel = 1
+        self.volumeLevel = 1
+        self.on = False
 
-    Methods:
-        turnOn(): None
-        turnOff(): None
-        getChannel(): int
-        setChannel(channel: int): None
-        getVolume(): int
-        setVolume(volumeLevel: int): None
-        channelUp(): None
-        channelDown(): None
-        volumeUp(): None
-        volumeDown(): None
+    #Methods:
+        #turnOn(): None
+        #turnOff(): None
+        #getChannel(): int
+        #setChannel(channel: int): None
+        #getVolume(): int
+        #setVolume(volumeLevel: int): None
+        #channelUp(): None
+        #channelDown(): None
+        #volumeUp(): None
+        #volumeDown(): None
 
 
-Create an instance of TV called tv1
-Create an instance of TV called tv2
+#Create an instance of TV called tv1
+#Create an instance of TV called tv2
 
-Turn on tv1
-Set tv1's channel to 30
-Set tv1's volume level to 3
+#Turn on tv1
+#Set tv1's channel to 30
+#Set tv1's volume level to 3
 
-Turn on tv2
-Set tv2's channel to 3
-Set tv2's volume level to 2
+#Turn on tv2
+#Set tv2's channel to 3
+#Set tv2's volume level to 2
 
-Print tv1's information
-Print tv2's information
+#Print tv1's information
+#Print tv2's information 

6b5b603f80d07b48ee2f9b99dfdd39dc4ca0fb6e
rizza <denostarizza43@gmail.com>
2023-05-18 07:42:20 +1200

    tv infos

---
+++ test_tv.py
@@ -0,0 +1,18 @@
+#Create an instance of TV called tv1
+#Create an instance of TV called tv2
+from tv import TV
+
+def print_tv_info(tv_name, channel, volume_level):
+    print(f"{tv_name}'s channel is {channel} and volume level is {volume_level}\n")
+
+
+#Turn on tv1
+#Set tv1's channel to 30
+#Set tv1's volume level to 3
+
+#Turn on tv2
+#Set tv2's channel to 3
+#Set tv2's volume level to 2
+
+#Print tv1's information
+#Print tv2's information
--- tv.py
+++ tv.py
@@ -1,62 +1,41 @@
-#Class TV:
-    #Properties:
-        #channel: int
-        #volumeLevel: int
-        #on: bool
-def __init__(self):
+class TV:
+    def __init__(self):
         self.channel = 1
         self.volumeLevel = 1
         self.on = False
 
-    #Methods:
-        #turnOn(): None
-        def turnOn(self):
-            self.on = True
-        #turnOff(): None
-        def turnOff(self):
-            self.on = False
-        #getChannel(): int
-        def getChannel(self):
-            return self.channel
-        #setChannel(channel: int): None
-        def setChannel(self, channel):
-            if self.on and 1 <= channel <= 120:
-                self.channel = channel
-        #getVolume(): int
-        def getVolume(self):
-            return self.volumeLevel
-        #setVolume(volumeLevel: int): None
-        def setVolume(self, volumeLevel):
-            if self.on and 1 <= volumeLevel <= 7:
-                self.volumeLevel = volumeLevel
-        #channelUp(): None
-        def channelUp(self):
-            if self.on and self.channel < 120:
-                self.channel += 1
-        #channelDown(): None
-        def channelDown(self):
-            if self.on and self.channel > 1:
-                self.channel -= 1
-        #volumeUp(): None
-        def volumeUp(self):
-            if self.on and self.volumeLevel < 7:
-                self.volumeLevel += 1
-        #volumeDown(): None
-        def volumeDown(self):
-            if self.on and self.volumeLevel > 1:
-                self.volumeLevel -= 1
+    def turnOn(self):
+        self.on = True
 
+    def turnOff(self):
+        self.on = False
 
-#Create an instance of TV called tv1
-#Create an instance of TV called tv2
+    def getChannel(self):
+        return self.channel
 
-#Turn on tv1
-#Set tv1's channel to 30
-#Set tv1's volume level to 3
+    def setChannel(self, channel):
+        if self.on and 1 <= channel <= 120:
+            self.channel = channel
 
-#Turn on tv2
-#Set tv2's channel to 3
-#Set tv2's volume level to 2
+    def getVolume(self):
+        return self.volumeLevel
 
-#Print tv1's information
-#Print tv2's information 
+    def setVolume(self, volumeLevel):
+        if self.on and 1 <= volumeLevel <= 7:
+            self.volumeLevel = volumeLevel
+
+    def channelUp(self):
+        if self.on and self.channel < 120:
+            self.channel += 1
+
+    def channelDown(self):
+        if self.on and self.channel > 1:
+            self.channel -= 1
+
+    def volumeUp(self):
+        if self.on and self.volumeLevel < 7:
+            self.volumeLevel += 1
+
+    def volumeDown(self):
+        if self.on and self.volumeLevel > 1:
+            self.volumeLevel -= 1

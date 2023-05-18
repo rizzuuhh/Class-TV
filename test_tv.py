a32c837c731233fc7b98770d7c451c363e6f34f9
rizza <denostarizza43@gmail.com>
2023-05-18 21:26:14 +1200

    creaated another file to test TV

--- test_tv.py
+++ test_tv.py
@@ -5,7 +5,6 @@ from tv import TV
 def print_tv_info(tv_name, channel, volume_level):
     print(f"{tv_name}'s channel is {channel} and volume level is {volume_level}\n")
 
-
 #Turn on tv1
 #Set tv1's channel to 30
 #Set tv1's volume level to 3

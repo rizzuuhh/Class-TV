c51e5994676446e8cd42fd66ae3f93064ad815e8
rizza <denostarizza43@gmail.com>
2023-05-18 21:35:25 +1200

    turned on TV1

--- test_tv.py
+++ test_tv.py
@@ -5,9 +5,14 @@ from tv import TV
 def print_tv_info(tv_name, channel, volume_level):
     print(f"{tv_name}'s channel is {channel} and volume level is {volume_level}\n")
 
+tv1 = TV()
+tv2 = TV()
 #Turn on tv1
+tv1.turnOn()
 #Set tv1's channel to 30
 #Set tv1's volume level to 3
+
+
 
 #Turn on tv2
 #Set tv2's channel to 3
